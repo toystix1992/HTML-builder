b75b0c51cccd0a2275e161c1e1782605733cdd9f
Parfionov Anton <toystix1992@gmail.com>
2021-11-03 22:01:06 +0300

    feat: add solution to the task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const path = require('path');
+const fs = require('fs');
+const pathToReadFile = path.join(__dirname, 'files/');
+const pathTowriteFile = path.join(__dirname, 'files-copy/');
+
+
+const copyFilesInDirectory = () => {
+    fs.mkdir(pathTowriteFile, { recursive: true }, err => {
+        if (err) throw err;
+        console.log('Все папки успешно созданы');
+    });
+    fs.readdir(pathToReadFile, (err, files) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        files.forEach(file => {
+            fs.copyFile(`${pathToReadFile}${file}`,
+                `${pathTowriteFile}${file}`, (err) => {
+                    if (err) throw err;
+                    console.log('files was copied');
+                });
+        })
+    });
+}
+
+copyFilesInDirectory();
+
+
 
