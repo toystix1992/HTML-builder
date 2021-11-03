edb52cf300f254a1984c45a718715b9f269c4a4f
Parfionov Anton <toystix1992@gmail.com>
2021-11-03 15:43:27 +0300

    fix: make a right solution to the task with module readline.

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,39 +1,39 @@
 const path = require('path');
 const fs = require('fs');
 const process = require('process');
-
 const pathToFile = path.join(__dirname, 'text.txt');
+const wStream = fs.createWriteStream(pathToFile);
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 fs.open(pathToFile, 'w', (err) => {
-  if(err) throw err;
+  if (err) throw err;
 });
 
 
-
-
-// process.on('beforeExit', () => {
-//   console.log('goodbye');
-// });
-function stopWriteText () {
-  console.log("goodbye");
-  process.exit();
+function stopWriteText() {
+  console.log('aaaaa, dude are you serious???');
+  rl.close();
 }
-
 
 
 function addText() {
-  process.stdin.resume();
-  console.log("Tel me something!");
-  process.stdin.on( "data", function(data) {
-    console.log("something more!");
-    fs.appendFile(pathToFile, data, function(error){
-      if(error) throw error;
-    });
-  });
-  process.on('exit', () => {
-    console.log('safsaf');
+  rl.question('who is the best of the best of the best? ', (userInput) => {
+    if (userInput.trim().toLowerCase() === 'exit') {
+      stopWriteText();
+    } else {
+      wStream.write(`${userInput} \n`);
+      addText();
+    }
   });
 }
 
+rl.on('close', () => {
+  console.log('\n see you soon!');
+});
 addText();
 
 
