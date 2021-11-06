c0b328a47389c183f85141cb1ba3742c96e4a4a3
Parfionov Anton <toystix1992@gmail.com>
2021-11-06 09:05:41 +0300

    feat: add features

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,10 +2,12 @@ const path = require('path');
 const fs = require('fs');
 
 const pathToReadCssFiles = path.join(__dirname, 'styles/');
+const pathToReadHtmlComponentsFiles = path.join(__dirname, 'components/');
+const pathToReadHtmlTemplateFiles = path.join(__dirname, 'template.html');
 const pathToReadAssetsFiles = path.join(__dirname, 'assets/');
 const pathToWriteCssFiles = path.join(__dirname, 'project-dist/');
 const pathToWriteAssetsFiles = path.join(__dirname, 'project-dist/assets/');
-let writeableStream = fs.createWriteStream(`${pathToWriteCssFiles}style.css`);
+const writeableStream = fs.createWriteStream(`${pathToWriteCssFiles}style.css`);
 
 fs.mkdir(pathToWriteCssFiles, { recursive: true }, err => {
     if (err) throw err;
@@ -56,8 +58,33 @@ const copyAssetsDirectory = (pathToReadAssetsFiles, pathToWriteAssetsFiles) => {
             });
         });
     }
-    )
+    );
 };
+
+function makeHtmlBundle() {
+    fs.copyFile(pathToReadHtmlTemplateFiles,
+    `${pathToWriteCssFiles}index.html`, (err) => {
+        if (err) throw err;
+        console.log('Template file was copied');
+    });
+    writebleStreamMain = fs.createWriteStream()
+    fs.readdir(pathToReadHtmlComponentsFiles, (err, files) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        files.forEach(file => {
+            console.log(file);
+            let readableStreamComponents = fs.createReadStream(`${pathToReadHtmlComponentsFiles}${file}`, "utf8");
+            readableStreamComponents.pipe(`${pathToWriteCssFiles}index.html`);
+        });
+    });
+
+    // const readableStreamIndexHtml = fs.createReadStream(`${pathToWriteCssFiles}index.html`, "utf8");
+    // readableStreamIndexHtml.on('data', function (chunk) {
+    // });
+}
 
 copyAssetsDirectory(pathToReadAssetsFiles, pathToWriteAssetsFiles);
 makeCssBundle();
+makeHtmlBundle();
